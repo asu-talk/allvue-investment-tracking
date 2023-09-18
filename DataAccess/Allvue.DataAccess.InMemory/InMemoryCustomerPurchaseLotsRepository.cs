bade9ecc71edeeec9b8dcdffe10af36fe6b582d2
Alexey Sushkevych <asushkevych@gmail.com>
2023-09-18 21:14:16 +0300

    Add in memory ICustomerPurchaseLotsRepository implementation

---
+++ DataAccess/Allvue.DataAccess.InMemory/InMemoryCustomerPurchaseLotsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+using Allvue.DataAccess.Contracts;
+using Allvue.DataAccess.Contracts.Dtos;
+
+namespace Allvue.DataAccess.InMemory;
+
+// Note: usually implementation is 'internal' and registered in DI as module (Autofac for example) or in some other way.
+public class InMemoryCustomerPurchaseLotsRepository : ICustomerPurchaseLotsRepository
+{
+    private readonly ConcurrentQueue<LotDto> _purchaseLots = new();
+
+    public async Task WritePurchaseLotAsync(LotDto purchaseLot, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+        _purchaseLots.Enqueue(purchaseLot);
+    }
+
+    public async Task<IReadOnlyCollection<LotDto>> ReadOrderedPurchaseLotsAsync(CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+        var result = _purchaseLots.ToArray();
+        return result;
+    }
+}
